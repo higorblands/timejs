b620e3c720fb155a3169c1576bb35349f21de81f
higorblands <67286451+higorblands@users.noreply.github.com>
2020-08-24 19:20:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function carregar(){
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
-    //var hora = data.getHours();
-    var hora = 21
+    var hora = data.getHours();
+  
 
     var minuto = data.getMinutes();
     if (hora >= 0 && hora < 12){
